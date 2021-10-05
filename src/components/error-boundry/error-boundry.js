7655ba84e8b297d9693fe79f8a48e25c9f8935a4
kozlov-oe <O.Kozlov@otpbank.ru>
2021-10-06 00:26:40 +0300

    added logic in app

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react';
 import Error from '../error';
 
 export default class ErrorBoundry extends Component {
+
+    state = {
+        isError: false
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({isError: true});
+    }
+
     render() {
-       
-        return 1;
+        if (this.state.isError) {
+            return(<Error/>);
+        }
+
+        return this.props.children;
     }
 }
